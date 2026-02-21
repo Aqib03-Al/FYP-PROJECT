09f919b93a3eb1751f3a57d16365a7ff6d0a060e
Aqib Ali <Axtylishaqib12345@gmail.com>
2026-02-21 21:35:30 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AIHealthTestSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
